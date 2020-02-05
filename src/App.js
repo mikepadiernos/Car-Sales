b0cf3576bd8cf939a90c090fa81623aa180d2ef1
Michael Padiernos <mike.padiernos@gmail.com>
2020-02-04 22:54:06 -0500

    FEATURE:(Reducer & Actions): established files
    - created index.js for reducers & actions
    - started on "BUY_ITEM" case

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
 const App = () => {
+
   const state = {
     additionalPrice: 0,
     car: {
